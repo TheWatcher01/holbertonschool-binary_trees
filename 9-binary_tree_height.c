e1c6aed57dcdec0d339c59ad6ef79fe61a9f9919
TheWatcher01 <teddydeberdt@gmail.com>
2024-01-10 13:13:34 +0100

    9-binary_tree_height.c_V2 + add ..gitignore file for all main.c tests files

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,20 +8,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+	size_t left_height, right_height;
 
 	/* If tree is NULL or a leaf, return 0 */
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
 	/* Recursively calculate the height of the left subtree */
-	if (tree->left)
-		left_height = binary_tree_height(tree->left) + 1;
+	left_height = binary_tree_height(tree->left);
 
 	/* Recursively calculate the height of the right subtree */
-	if (tree->right)
-		right_height = binary_tree_height(tree->right) + 1;
+	right_height = binary_tree_height(tree->right);
 
-	/* Return the greater height between the two subtrees */
-	return (left_height > right_height ? left_height : right_height);
+	/* Return the greater height between the two subtrees, plus one for the current node */
+	return (left_height > right_height ? left_height : right_height) + 1;
 }
